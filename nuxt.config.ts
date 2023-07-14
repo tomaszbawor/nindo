ff32722eb8658f0f44d414eeaaf26b9464185f5f
Tomasz Bawor <bawortomasz@gmail.com>
2023-07-15 01:11:26 +0200

    Add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false
+  ssr: false,
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
 })
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div :class="$style.testStyle">INDEX</div>
+</template>
+<style lang="scss" module>
+.testStyle {
+    color: red;
+}
+</style>
