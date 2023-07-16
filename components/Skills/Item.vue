6fd7daad759e26f634a6ac6dead0d2de415e2e77
Tomasz Bawor <bawortomasz@gmail.com>
2023-07-16 03:00:36 +0200

    Fix styles

--- components/Skills/Item.vue
+++ components/Skills/Item.vue
@@ -13,27 +13,26 @@ export interface PlayerSkillItemProps {
 }
 </script>
 <template>
-    <div class="card bg-base-200 shadow-xl">
-        <div class="card-body">
-            <div>
-                <div>
-                    <div>
-                        <b>
-                            {{ name }}
-                        </b>
-                        <div class="float-right">
-                            <div class="badge badge-sm badge-secondary flex-end">
-                                {{ props.level }}
-                            </div>
-                        </div>
-                    </div>
+    <div class="card bg-base-200 shadow-xl h-full">
+        <div class="card-body flex flex-col h-full">
 
+            <div>
+                <div class="flex flex-row justify-between">
+                    <b class="text-sm">
+                        {{ name }}
+                    </b>
+                    <div class="badge badge-sm badge-secondary">
+                        {{ props.level }}
+                    </div>
                 </div>
-                <div class="text-xs">{{ props.description }}</div>
+
+            </div>
+            <div class="text-xs">{{ props.description }}</div>
+
+            <div class="mt-auto">
                 <div class="text-secondary text-sm">{{ currentExpStr }} / {{ maxExpStr }} Exp</div>
                 <progress class="progress progress-secondary w-full" :value="currExp" :max="maxExp"></progress>
             </div>
-
         </div>
     </div>
 </template>
